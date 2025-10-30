ad5854bb4b0865d9f64a06683c468dbd327ab3f5
Md. Nahidul Islam Opu <opu.nahidul@gmail.com>
2025-10-30 18:41:09 -0500

    Create modules

---
+++ modules/constants.py
@@ -0,0 +1,38 @@
+AIDEV_DATA_DIR = "AIDev/"
+OUTPUT_DIR = "Outputs/"
+
+PREFER_ORDER = ["Human", "OpenAI_Codex", "Devin", "Copilot", "Cursor", "Claude_Code"]
+
+class FileName:
+    ALL_PULL_REQUEST = "all_pull_request"
+    ALL_REPOSITORY = "all_repository"
+    ALL_USER = "all_user"
+
+    HUMAN_PR_TASK_TYPE = "human_pr_task_type"
+    HUMAN_PULL_REQUEST = "human_pull_request"
+
+    POP_PULL_REQUEST = "pull_request"
+    POP_REPOSITORY = "repository"
+    POP_USER = "user"
+
+    POP_ISSUE = "issue"
+    POP_RELATED_ISSUE = "related_issue"
+
+    POP_PR_TIMELINE = "pr_timeline"
+    POP_PR_REVIEWS = "pr_reviews"
+    POP_PR_TASK_TYPE = "pr_task_type"
+
+    POP_PR_COMMENTS = "pr_comments"
+    POP_PR_REVIEW_COMMENTS_V2 = "pr_review_comments_v2"
+    POP_PR_REVIEW_COMMENTS = "pr_review_comments"
+
+    POP_PR_COMMIT_DETAILS = "pr_commit_details"
+    POP_PR_COMMITS = "pr_commits"
+
+
+
+
+
+
+
+
---
+++ modules/utilities.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import matplotlib as mpl
+
+from modules.constants import *
+
+COLOR_MAP = {
+    "Human": "#56B4E9",
+    "OpenAI_Codex": "#D55E00",
+    "OpenAI Codex": "#D55E00",
+    "Codex": "#F0E442",
+    "Devin": "#009E73",
+    "Copilot": "#0072B2",
+    "GitHub Copilot": "#0072B2",
+    "Cursor": "#785EF0",
+    "Claude_Code": "#DC267F",
+    "Claude Code": "#DC267F",
+}
+
+
+# –– Nature-ready styling
+mpl.rcParams.update(
+    {
+        "font.family": "serif",
+        "font.serif": ["Times New Roman", "Times"],
+        "mathtext.fontset": "stix",
+        "axes.linewidth": 1.0,
+        "axes.labelsize": 14,
+        "axes.titlesize": 14,
+        "xtick.major.size": 4,
+        "ytick.major.size": 4,
+        "xtick.labelsize": 14,
+        "ytick.labelsize": 14,
+        "legend.fontsize": 14,
+        "legend.title_fontsize": 14,
+        "figure.dpi": 300,
+    }
+)
+
+def read_aidev(name):
+    return pd.read_parquet(AIDEV_DATA_DIR + name + ".parquet")

7e04a0c5c8f743043450600e0bec6711f7fc4cdb
Md. Nahidul Islam Opu <opu.nahidul@gmail.com>
2025-11-20 11:23:14 -0600

    Update filter.py

--- filter.py
+++ filter.py
@@ -2,23 +2,30 @@
 
 import os
 from tqdm import tqdm
-
+import pandas as pd
 from modules.constants import *
 from modules.utilities import *
 from modules.text_classifiers import *
 
-random_sate = 5
+random_state = 5
 
 # %%
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 
 # %%
-def filter_performance_topics(filename, columns, preprocessor, classifier):
+def filter_performance_topics(filename, columns, preprocessor, classifier, callback, skip = 0):
     df = read_aidev(filename)
     output_rows = []
 
+    count = 0
+
     for _, row in tqdm(df.iterrows()):
+        if count < skip:
+            continue
+        
+        count = count + 1
+        
         text = ""
         for col in columns:
             if isinstance(row[col], str):
@@ -32,11 +39,18 @@ def filter_performance_topics(filename, columns, preprocessor, classifier):
                 text += val
 
         if text and text != "" and classifier.classify(text):
-            output_rows.append(row)
-            try:
+            row = row.copy()
+
+            if isinstance(classifier, RegexClassifier) or isinstance(classifier, SimpleSubstringClassifier):
                 row["matched_words"] = classifier.get_matches()
-            except:
-                pass
+            elif isinstance(classifier, LLMClassifier):
+                row["llm_output"] = classifier.get_prediction()
+            elif isinstance(classifier, EmbeddingClassifier):
+                row["probability"] = classifier.get_score()
+
+            output_rows.append(row)
+            
+        callback(pd.DataFrame(output_rows))
 
     output_df = pd.DataFrame(output_rows)
 
@@ -44,6 +58,9 @@ def filter_performance_topics(filename, columns, preprocessor, classifier):
 
 
 # %%
+
+def save(df):
+    df.to_csv(os.path.join(OUTPUT_DIR, "llm_filtered.csv"), index=False)
 
 filename = FileName.ALL_PULL_REQUEST
 columns = ["title", "body"]
@@ -90,5 +107,4 @@ prompt = [
 classifier = LLMClassifier(prompt)
 preprocessor = None
 
-df = filter_performance_topics(filename, columns, preprocessor, classifier)
-df.to_csv(os.path.join(OUTPUT_DIR, "llm_filtered.csv"), index=False)
+df = filter_performance_topics(filename, columns, preprocessor, classifier, save)
